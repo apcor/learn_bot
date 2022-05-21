9c55d5131d0cc81484c34a518796acf7618f3988
Arsen Sogoyan <apcor@yandex.ru>
2022-05-21 18:48:32 +0300

    Added ReadMe.md; Added planet constellation output feature

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import logging
+import ephem
+import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -17,13 +19,27 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+def const_planet(update, context):
+    user_pl = update.message.text.split()[1].lower().capitalize()
+    loc_date = datetime.date.today()
+    if user_pl in planets:
+        user_pl_eph = getattr(ephem, user_pl)
+        user_pl_const = ephem.constellation(user_pl_eph(loc_date.strftime("%Y/%m/%d")))
+        update.message.reply_text(f'Today is {loc_date.strftime("%A, %d of %B %Y")}. \n{user_pl} is in the constellation {user_pl_const[1]}.')
+    else:
+        update.message.reply_text('Please insert a valid name of a planet (other than Earth)')
+
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', const_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
 
     logging.info('The bot has been initiated...')
 
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Arsen's Telegram bot
+
+## General info
+
+This bot was created by **Arsen Sogoyan** during the [Learn Python course](https://learn.python.ru/) (*14/05/2022 - 16/07/2022*).
+
+The bot is accessible on Telegram via [link](https://t.me/LearnPythonArsensBot).
+
+**Note:** currently the bot works _only_ when `bot.py` module is running on my local machine. 
+
+## Features
+
+- ### Echo
+Passing a `/start` command to the bot initiates *echo* mode: every input by the user is replicated by the bot in its response.
+
+- ### Planet constellation
+
+Passing a `/planet` command followed by a name of a planet (other than Earth) prompts a reply with *today's date* and *constellation the planet is in*.
+
